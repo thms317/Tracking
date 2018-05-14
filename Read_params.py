f0f2899a3475d4493bbf8b1159548f9fdd41366b
thms317 <brouwer.thomas@gmail.com>
2018-05-14 15:40:54 +0200

    The program needs to be cleaned, however the fitting is working

--- Read_params.py
+++ Read_params.py
@@ -2,11 +2,23 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+# function to reject outliers, replace with NaN
+def reject_outliers(data):
+    data_filtered = []
+    norm_data = []
+    norm_data = abs(data - np.mean(data))
+    for n, i in enumerate(norm_data):
+        if i < 1 * np.std(data):
+            i = data[n]
+        data_filtered.append(i)
+    return data_filtered
+
 plt.rcParams.update({'font.size': 28})
 
 titles_file = "C:\\Users\\brouw\\Google Drive\\Bead images\\MyOne\\unknown\\titles.txt"
 parameters_files = "C:\\Users\\brouw\\Google Drive\\Bead images\\MyOne\\Fitted Parameters\\unknown\\"
 parameters_files = "C:\\Users\\brouw\\Google Drive\\Bead images\\MyOne\\Fitted Parameters\\150915_005\\"
+parameters_files = "C:\\Users\\brouw\\Desktop\\FOV\\Fit Parameters\\5\\"
 parameters_list = os.listdir(parameters_files)
 
 f = open(titles_file, 'r')
@@ -25,11 +37,18 @@ for file in parameters_list:
         params[n] = param.strip('\n')
     acc_par.append(params)
 
-parameter = 1
-hist=[]
-for par in acc_par:
-    hist.append(float(par[parameter]))
-print(hist)
-plt.hist(hist)
-plt.title(titles[parameter]+" // mean = "+str(np.mean(hist)))
-plt.show()
+print("Number of fitted beads: "+str(len(parameters_list)))
+
+parameters = [1,5,6,7,10]
+for parameter in parameters:
+    hist=[]
+    for par in acc_par:
+        hist.append(float(par[parameter]))
+    # print(hist)
+    # hist = reject_outliers(hist)
+    # print(hist)
+    print(titles[parameter]+" - Median = "+str(np.median(hist)))
+
+# plt.hist(hist)
+# plt.title(titles[parameter]+" // mean = "+str(np.mean(hist)))
+# plt.show()
---
+++ functions.py
@@ -0,0 +1,177 @@
+import numpy as np
+from scipy.signal import argrelextrema
+from scipy import stats
+
+kT = 4.114  # pN nm
+L0 = 0.34  # nm / bp
+
+
+# worm-like chain
+def WLC(f, L_bp=1e3, P_nm=50, S_pN=1e3, z0_nm=0, HMf_cWLC = 0):
+    z = 1 - 0.5 * np.sqrt(kT / (f * P_nm)) + f / S_pN
+    z *= L_bp * 0.34
+    z += z0_nm
+    # w = f dz= f*z - z df
+    w = f * z - L_bp * L0 * (f - np.sqrt((f * kT) / P_nm) + f ** 2 / (2 * S_pN))
+    return z, w / kT
+
+def WLC_fit(f, p, L, S, z0):
+    kT = 4.114  # (pN nm) - Boltzmann factor
+    return (L * (1 - 0.5 * (np.sqrt(kT / (f * p))) + f / S)) / 1000 + z0
+
+
+# freely jointed chain
+def FJC(f, b=None, k_pN_nm=0.01, L_nm=5, z0_nm=0, HMf_cFJC = 0):
+    if b == None:
+        b = 3 * kT / (k_pN_nm * L_nm)
+    x = f * b / kT
+    # coth(x)= (exp(x) + exp(-x)) / (exp(x) - exp(-x)) --> see Wikipedia
+    exp_x = np.exp(x)
+    z = (exp_x + (1 / exp_x)) / (exp_x - (1 / exp_x)) - 1 / x
+    z *= L_nm
+    z += z0_nm
+    # w = f dz= f*z - z df
+    # z = coth(x) - 1/x
+    z_df = L_nm * (kT / b) * (np.log(np.sinh(x)) - np.log(x))
+    # z_df = L_nm * (kT / b) * (np.log(np.sinh(x)) - np.log(x)) # 4 pi missing
+    w = f * z - z_df
+    return z, w / kT
+
+
+# L_app - function modified
+def kCalcP(L_bp, P_nm=50, N=0, alpha=0):
+    #   Persistence length correction according to Kulic and Schiessel
+    C = 8 * (1 - np.cos(alpha * (np.pi / 180) / 4.0))
+    ro = N / (L_bp * L0)
+    return P_nm / ((1 + P_nm * N * C * ro) ** 2)
+
+
+# degrees to radians
+def degrad(deg):
+    return deg * (np.pi / 180)
+
+
+# radians to degrees
+def raddeg(rad):
+    return rad * (180 / np.pi)
+
+
+# calculate force
+def calc_force(i):
+    A = 85  # for 2.8 um beads (pN)
+    l1 = 1.4  # decay length 1 (mm)
+    l2 = 0.8  # decay length 2 (mm)
+    f0 = 0.01  # force-offset (pN)
+    return A * (0.7 * np.exp(-i / l1) + 0.3 * np.exp(-i / l2)) + f0
+
+
+# median filter
+def medfilt(x, k):  # Apply a length-k median filter to a 1D array x. Boundaries are extended by repeating endpoints.
+    assert k % 2 == 1, "Median filter length must be odd."
+    assert x.ndim == 1, "Input must be one-dimensional."
+    k2 = (k - 1) // 2
+    y = np.zeros((len(x), k), dtype=x.dtype)
+    y[:, k2] = x
+    for i in range(k2):
+        j = k2 - i
+        y[j:, i] = x[:-j]
+        y[:j, i] = x[0]
+        y[:-j, -(i + 1)] = x[j:]
+        y[-j:, -(i + 1)] = x[-1]
+    return np.median(y, axis=1)
+
+
+# function to reject outliers, replace with NaN
+def reject_outliers(data):
+    data_filtered = []
+    norm_data = []
+    norm_data = abs(data - np.mean(data))
+    for n, i in enumerate(norm_data):
+        if i > 2 * np.std(data):
+            i = np.nan
+        else:
+            i = data[n]
+        data_filtered.append(i)
+    return data_filtered
+
+
+# get numbers from string
+def get_num(x):
+    return float(''.join(ele for ele in x if ele.isdigit() or ele == '.'))
+
+
+def peak_finder(y):  # Finds y peaks at position x in xy graph
+    y = np.array(y)
+    # x=list(x)
+
+    # mirror the data in the Y-axis (to find potential peak at x=0)
+    # x_x = list(reversed(np.negative(np.array(x[1:])))) + x
+    Yy = np.append(y[:-1], y[::-1])
+    yYy = np.append(y[::-1][:-1], Yy)
+
+    from scipy.signal import argrelextrema
+    maxInd = argrelextrema(np.array(yYy), np.greater)
+    r = np.array(yYy)[maxInd]
+    a = maxInd[0]
+
+    # discard all peaks for negative dimers
+    peaks_index = []
+    peaks_height = []
+    for n, i in enumerate(a):
+        i = 1 + i - len(y)
+        if i >= 0 and i <= len(y):
+            peaks_height.append(r[n])
+            peaks_index.append(i)
+
+    return peaks_index, peaks_height
+
+
+# fitting two lines
+def two_lines(x, a, b, c, d):
+    one = a * x + b
+    two = c * x + d
+    return np.maximum(one, two)
+
+
+# calculate drift using stuck bead
+def drift_stuck(data_lines, headers, time, beads):
+    # find a stuck bead by RMS analysis of single bead in Z-direction
+    z_rms = []
+    for b in range(0, beads):
+        z_temp = []
+        for x in data_lines:
+            z_temp.append(float(x.split()[headers.index('Z' + str(b) + ' (um)')]))
+        z_temp = np.array(z_temp)
+        z_temp -= np.mean(z_temp)
+        z_rms.append(np.sqrt(np.mean(z_temp ** 2)))
+    stuck_index = int(z_rms.index(min(z_rms)))
+
+    z_stuck = []
+    for x in data_lines:
+        z_stuck.append(float(x.split()[headers.index('Z' + str(stuck_index) + ' (um)')]))
+
+    # correcting the drift using a stuck bead
+    driftz = []
+    driftt = []
+    minmax = []
+    for n, z in enumerate(z_stuck):
+        driftt.append(time[n])
+        driftz.append(z * 1000)
+    minmax.append(np.percentile(driftz[:100], 1))
+    minmax.append(np.percentile(driftz[-100:], 1))
+    minmax.append(np.percentile(driftt[:100], 1))
+    minmax.append(np.percentile(driftt[-100:], 1))
+    slope, intercept, r_value, p_value, std_err = stats.linregress([minmax[2], minmax[3]], [minmax[0], minmax[1]])
+
+    return slope
+
+
+# calculate drift using self
+def drift_self(Z, time):
+    # correcting the drift using a stuck bead
+    Z_start = np.percentile(Z[:100] * 1000, 1)
+    Z_end = np.percentile(Z[-100:] * 1000, 1)
+    t_start = np.percentile(time[:100], 1)
+    t_end = np.percentile(time[-100:], 1)
+    drift_slope, intercept, r_value, p_value, std_err = stats.linregress([t_start, t_end], [Z_start, Z_end])
+    return drift_slope
