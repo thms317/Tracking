649af012c80b6bd787dcf4093f9e0987685c6222
thms317 <brouwer.thomas@gmail.com>
2018-07-31 13:43:45 +0200

    Fancy boxplots + working on Allan Variance

--- TrackBin_AllanVariance.py
+++ TrackBin_AllanVariance.py
@@ -9,16 +9,20 @@ import matplotlib.cm as cm
 correct_global_drift = True
 correct_reference_beads = False
 
-
-trackbin_path = "C:\\Users\\brouw\\Desktop\\TrackBin test data\\"
+trackbin_path = "C:\\Users\\tbrouwer\\Desktop\\TrackBin test data\\"
 
 trackbin_files = []
 os.chdir(trackbin_path)
 for file in glob.glob("*.dat"):
     trackbin_files.append(file)
 
+A, B, matrix = [], [], []
 
 for n, file in enumerate(trackbin_files):
+
+    # What's the frequency Kenneth?
+    A.append(file[10:13])
+    B.append(file[15:18])
 
     # files
     datfile = trackbin_path + file
@@ -28,7 +32,7 @@ for n, file in enumerate(trackbin_files):
     headers = list(df)
 
     # get number of beads
-    beads = headers[len(headers)-1]
+    beads = headers[len(headers) - 1]
     beads = func.get_int(beads)
 
     # correct global drift
@@ -46,11 +50,11 @@ for n, file in enumerate(trackbin_files):
 
     drift = float(np.median(drift))
 
-    print("Processing file: " + str(file) +" (drift: " + str(round(drift,2)) + " nm/s)")
+    print("Processing file: " + str(file) + " (drift: " + str(round(drift, 2)) + " nm/s)")
 
-    for n in range(beads):
+    for bead in range(beads):
 
-        print("Processing bead " + str(n))
+        # print("Processing bead " + str(n))
 
         Z_meas = "Z" + str(n) + " (um)"
         Z = np.array(df[Z_meas])
@@ -62,83 +66,41 @@ for n, file in enumerate(trackbin_files):
             # do Klaas script
             print("Reference Bead uncorrected")
 
-        '''
-        # Allan Variance
-        i = 100  # start interval at i
-        m = 1000  # interval of length m
-        N = len(time)  # total number of datapoints
-        tau = freq * m
-        t_acq = freq * N
-        # sigma squared
-        sigma_sq = 0.5 * ((np.mean(Z[i:i+m+1])-np.mean(Z[i:i+m]))**2)
-        # standard error (identical) - valid for tau << t_acq
-        SE = np.sqrt(m/N) * np.sqrt(sigma_sq)
-        SE = np.sqrt((tau)/(t_acq))*np.sqrt(sigma_sq)
-        # for tau < t_acq
-        n_approx = N / m
-        SE_approx = (1 / np.sqrt(n)) * np.sqrt(sigma_sq)
-        print(sigma_sq, np.sqrt(sigma_sq), SE, SE_approx)
-        # neighboring intervals
-        '''
-
         # Allan Variance (Van Oene paper)
         N = len(time)  # total number of datapoints
         T_meas = N / freq  # total measurement time
         M = 10  # split the data in segments
-        m = int(N/M)  # length of interval (rolling window)
-
-        colors = iter(cm.rainbow(np.linspace(0, 1, M)))
+        m = int(N / M)  # length of interval (rolling window)
 
         # simple AV
 
+        colors_simple = iter(cm.rainbow(np.linspace(0, 1, M)))
+
         AV = []
 
-        for i in range(M-1):
+        for i in range(M - 1):
+            interval_i = Z[i * m:(1 + i) * m]  # interval i
+            interval_j = Z[(1 + i) * m:(2 + i) * m]  # interval j
+            x_i = time[i * m:(1 + i) * m]  # time axis i
+            x_j = time[(1 + i) * m:(2 + i) * m]  # time axis j
 
-            interval_i = Z[i*m:(1+i)*m]  # interval i
-            interval_j = Z[(1+i)*m:(2+i)*m]  # interval j
-            x_i = time[i*m:(1+i)*m]  # time axis i
-            x_j = time[(1+i)*m:(2+i)*m]  # time axis j
+            # plt.scatter(x_i,interval_i,color=next(colors_simple))
 
-            # plt.scatter(x_i,interval_i,color=next(colors))
+            AV.append((np.mean(interval_j) - np.mean(interval_i)) ** 2)
 
-            AV.append((np.mean(interval_j)-np.mean(interval_i))**2)
-
-        AV_tot = (1 / (2*(M-1))) * sum(AV)
-
-        # overlapping AV
-
-        AV_ov = []
-
-        for i in range(N + 1 - 2 * m):
-
-            interval_i = Z[i*m:(1+i)*m]  # interval i
-            interval_j = Z[(1+i)*m:(2+i)*m]  # interval j
-            x_i = time[i*m:(1+i)*m]  # time axis i
-            x_j = time[(1+i)*m:(2+i)*m]  # time axis j
-
-            # plt.scatter(x_i,interval_i,color=next(colors))
-
-            AV_ov.append((np.mean(interval_j)-np.mean(interval_i))**2)
-
-        AV_tot_ov = (1 / (2 * (N + 1 - 2 * m))) * sum(AV)
-
-        print(AV_tot)
-
-
-
-
-        # theta_i = np.mean(interval_i)
-        # theta_j = np.mean(interval_j)
-        #
-        # AV_int = 0.5 * ((theta_j - theta_i)**2)
-        # print(AV)
-
-        # plotting
-        # x = np.arange(m)*(1/freq)  # x-axis
-        # plt.scatter(x,interval_i, color = 'red')
-        # plt.scatter((1/freq)*m+x, interval_j, color = 'blue')
-        # plt.plot(time,Z,color='black')
         # plt.show()
 
-    break
+        AV_tot = (1 / (2 * (M - 1))) * sum(AV)
+
+        print(n, bead)
+        matrix.append(AV_tot)
+
+        # print(AV_tot)
+print(n)
+print(bead)
+
+matrix = [2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 2.4849633423955332e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 4.8469896117606538e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 3.1713488625138885e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 1.7341118386473719e-05, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 0.25893776483526576, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 4.7904304072476678e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 2.8268741984819365e-05, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 0.25351851547074877, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05, 3.0709955453280431e-05]
+
+print(matrix)
+matrix = np.array(matrix)
+matrix.reshape(n,bead)
