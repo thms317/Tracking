5eebb0225a90c18f79e158882f1441fc913f9708
thms317 <brouwer.thomas@gmail.com>
2018-10-10 17:11:09 +0200

    Added script to read Poissonian noise

---
+++ fitting_poisson_noise.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+folder = "S:\\Brouwer\\Camera Noise\\"
+file = "noise_double.txt"
+
+df = pd.read_csv(folder+file, sep="\t")
+
+pixel = np.array(df['Pixel Value - Histogram'])
+binned = np.array(df['# of Pixels - Histogram'])
+
+plt.plot(pixel, binned, 'o-')
+plt.show()
